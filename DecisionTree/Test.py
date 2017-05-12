c5c9aa95c1fe9667d3f2fef42bb7c95deac38003
kuangwentao <wt.kuang@icloud.com>
2017-05-12 22:28:35 +1200

    Testing Shannon Entropy Calculation

---
+++ DecisionTree/Test.py
@@ -0,0 +1,17 @@
+import numpy as np
+from Tree import *
+'''
+kNN: Testing Decision Tree
+
+@author: Wentao Kuang
+'''
+
+#Create Test data Set
+def createDataSet():
+    dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+group, labels = createDataSet()
+
+print ShannonEnt(group)
