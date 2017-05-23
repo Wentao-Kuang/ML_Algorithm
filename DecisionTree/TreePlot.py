62712a61cd2824e6bf994e22f7a93aa1f7c40af8
kuangwentao <wt.kuang@icloud.com>
2017-05-23 21:18:19 +1200

    Fix

--- DecisionTree/TreePlot.py
+++ DecisionTree/TreePlot.py
@@ -70,8 +70,3 @@ def createPlot(inTree):
 #    plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
 #    plt.show()
 
-def retrieveTree(i):
-    listOfTrees =[{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
-                  {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
-                  ]
-    return listOfTrees[i]
